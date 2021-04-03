978597ecfc6fbbe999bb4da0c4d8fd8c21f4befc
stryitg <stryitg@gmail.com>
2021-04-03 13:13:41 +0300

    Revert "Revert "multipixel sampling""
    This reverts commit 4c901c8862454cf1331de906e3c4274f9502fde2.

---
+++ src/NDTilingPixelSampler.h
@@ -0,0 +1,14 @@
+// #pragma once
+// 
+// #include <NormalDistribution.h>
+// 
+// class NDTilePixelSampler {
+// public:
+//     NDTilePixelSampler(const NormalDist& nd);
+// 
+//     Eigen::VectorXd Sample(const Eigen::VectorXd& tile_vals, size_t i) const;
+// 
+// private:
+// 
+// 
+// };
