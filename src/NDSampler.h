d839f8ed0be2cd9b4a49ce8e316525c41279b5fc
stryitg <stryitg@gmail.com>
2021-04-03 13:10:20 +0300

    blending

--- src/NDSampler.h
+++ src/NDSampler.h
@@ -2,11 +2,14 @@
 
 #include <NormalDistribution.h>
 #include <random>
+#include <memory>
 
 
 class NDSampler {
 public:
     NDSampler(const NormalDist& nd);
+    // NDSampler(const NDSampler&) = default;
+    NDSampler(NDSampler&&) = default;
     
     Eigen::VectorXd Sample() const;
     
@@ -14,10 +17,19 @@ private:
     Eigen::VectorXd GetStandartNDVec() const;
     
 private:
+    struct RNG {
+        RNG()
+        : rd()
+        , gen(rd()) {}
+        
+        std::random_device rd;
+        std::mt19937 gen;
+        std::normal_distribution<double> d;
+    };
+    
     NormalDist m_nd;
     Eigen::MatrixXd m_s_sqrt;
     
-    mutable std::random_device m_rd;
-    mutable std::mt19937 m_gen;
-    mutable std::normal_distribution<double> m_d;
+    mutable std::unique_ptr<RNG> m_rng;
+    
 };
